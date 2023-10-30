40f5a073800e7e5712dcafc9db20c1ece0d1be11
sukru.ciris <sukru.ciris@arcelik.com>
2023-10-30 19:44:56 +0300

    UINT_MAX defined if not

--- src/core/dynamic.h
+++ src/core/dynamic.h
@@ -1,21 +1,26 @@
 #pragma once
-//my own vector library
+// my own vector library
+
+#ifndef UINT_MAX
+#define UINT_MAX 4294967295U
+#endif
+
 typedef struct DA DA;
 
-DA* create_DA(unsigned int itemsize);
+DA *create_DA(unsigned int itemsize);
 
-void pushback_DA(DA* da, void* item);
+void pushback_DA(DA *da, void *item);
 
-void delete_DA(DA* da);
+void delete_DA(DA *da);
 
-void* get_data_DA(DA* da);
+void *get_data_DA(DA *da);
 
-unsigned int get_size_DA(DA* da);
+unsigned int get_size_DA(DA *da);
 
-unsigned int get_item_size_DA(DA* da);
+unsigned int get_item_size_DA(DA *da);
 
-void remove_DA(DA* da, unsigned int index);
+void remove_DA(DA *da, unsigned int index);
 
-void clear_DA(DA* da);
+void clear_DA(DA *da);
 
-unsigned int get_index_DA(DA* da, void* item);
+unsigned int get_index_DA(DA *da, void *item);
